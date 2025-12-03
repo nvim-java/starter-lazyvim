1385137aee85afba8d50fbf74dc5debdfa19bb37
s1n7ax <srineshnisala@gmail.com>
2025-12-04 03:32:17 +0530

    refactor: simplify java plugin config for v4.0.0

--- lua/plugins/java/init.lua
+++ lua/plugins/java/init.lua
@@ -1,45 +1,8 @@
 return {
   "nvim-java/nvim-java",
-  config = false,
-  dependencies = {
-    {
-      "neovim/nvim-lspconfig",
-      opts = {
-        servers = {
-          -- Your JDTLS configuration goes here
-          jdtls = {
-            -- settings = {
-            --   java = {
-            --     configuration = {
-            --       runtimes = {
-            --         {
-            --           name = "JavaSE-23",
-            --           path = "/usr/local/sdkman/candidates/java/23-tem",
-            --         },
-            --       },
-            --     },
-            --   },
-            -- },
-          },
-        },
-        setup = {
-          jdtls = function()
-            -- Your nvim-java configuration goes here
-            require("java").setup({
-              -- root_markers = {
-              --   "settings.gradle",
-              --   "settings.gradle.kts",
-              --   "pom.xml",
-              --   "build.gradle",
-              --   "mvnw",
-              --   "gradlew",
-              --   "build.gradle",
-              --   "build.gradle.kts",
-              -- },
-            })
-          end,
-        },
-      },
-    },
-  },
+  branch = "v4.0.0",
+  config = function()
+    require("java").setup()
+    vim.lsp.enable("jdtls")
+  end,
 }
